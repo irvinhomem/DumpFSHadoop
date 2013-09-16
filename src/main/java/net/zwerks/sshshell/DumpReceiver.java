08f341270643b353777cfb0b9b2f1ffd286bd9ba
patriotemeritus <patriotemeritus@gmail.com>
2013-09-16 18:15:58 +0200

    Just backing up the current work done ...

--- src/main/java/net/zwerks/sshshell/DumpReceiver.java
+++ src/main/java/net/zwerks/sshshell/DumpReceiver.java
@@ -7,12 +7,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.util.Progressable;
 
 
 public class DumpReceiver implements Runnable {
@@ -22,12 +23,14 @@ public class DumpReceiver implements Runnable {
     private int serverPort;
     private String OutputPath;
     private String outputFilename;
+    private String HDFSOutputPath;
     //private InputStream inStream;
 
 	public DumpReceiver(String FileDumpDir, String DumpFileName, int listenPort){
 		// TODO Auto-generated constructor stub
 		this.OutputPath = FileDumpDir;				//Directory where incoming file is to be dumped
 		this.outputFilename = DumpFileName;			//File name of incoming dump file
+		this.HDFSOutputPath = "/user/hadoop_user/";					//Hadoop HDFS Chosen Dump Directory
 		this.serverPort = listenPort;
 		this.connectedSock = null;
 		//this.inStream = null;
@@ -75,15 +78,42 @@ public class DumpReceiver implements Runnable {
 			
 	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 	        
+	        /*For Hadoop HDFS*/
+	        Configuration myHDFSConfig = new Configuration();
+	        myHDFSConfig.addResource("/HADOOP_HOME/conf/core-site.xml");
+	        myHDFSConfig.addResource("/HADOOP_HOME/conf/hdfs-site.xml");
+	        
+	        /*---*/
+	        
 	        
 	        if (inStream != null) {
 	        	FileOutputStream fos = null;
 	            BufferedOutputStream bos = null;
+	            
+	            /*For Hadoop HDFS*/
+	            //FSDataOutputStream hdfsout = null; 
+	            /*---*/
 	        	
 	        	System.out.println("Connected on: "+ this.connectedSock.getLocalAddress()+":"+this.connectedSock.getLocalPort());
 	        	System.out.println("Receiving file from: "+ this.connectedSock.getInetAddress()+":"+this.connectedSock.getPort());
 	            
 	            try {
+	                /*For Hadoop HDFS Capability*/
+	            	FileSystem hdfs = FileSystem.get(myHDFSConfig);
+	            	//hdfs = FileSystem.get(new URI("hdfs://localhost:54310"), myHDFSConfig);
+	            	//FSDataOutputStream hdfsout = hdfs.create(new Path(this.HDFSOutputPath + this.outputFilename));
+	            	Path theHDFSOuputPath = new Path(this.HDFSOutputPath + this.outputFilename);
+	            	System.out.println("HDFS Path to output to ... " + theHDFSOuputPath.toString());
+	            	if(hdfs.exists(theHDFSOuputPath)){
+	            		System.out.println("HDFS Output Path: " + theHDFSOuputPath.toString() + " EXISTS");
+	            	}
+	            	System.out.println("Current Working Directory is: " + hdfs.getWorkingDirectory());
+	            	System.out.println("Current Home Directory is: " + hdfs.getWorkingDirectory());
+	            	
+	            	FSDataOutputStream hdfsout = hdfs.create(theHDFSOuputPath);
+	                ////////hdfsout = new FSDataOutputStream(fos, null);
+	                /*---*/
+	            	
 	                fos = new FileOutputStream( this.OutputPath + this.outputFilename );
 	                System.out.println("------------------------------------------------");
 	                System.out.println("Beginning File-write to disk ...");
@@ -94,6 +124,7 @@ public class DumpReceiver implements Runnable {
 	                //bos.
 	                
 	                bos = new BufferedOutputStream(fos);
+
 	                //System.out.println("Preparing to write");
 	                
 	                bytesRead = inStream.read(aByte, 0, aByte.length);
@@ -108,6 +139,10 @@ public class DumpReceiver implements Runnable {
 	                do {
 	                        baos.write(aByte);
 	                        bytesRead = inStream.read(aByte);
+	                        
+	                        /*For Hadoop HDFS*/
+	                        hdfsout.write(aByte, 0, aByte.length);
+	                        /*---*/
 	                        
 	                        //Some sort of feedback that stuff is happening
 	                        byteCounter++;
@@ -132,14 +167,25 @@ public class DumpReceiver implements Runnable {
 	                bos.flush();
 	                bos.close();
 	                
+	                /*For Hadoop HDFS*/
+	                hdfsout.close();
+	                hdfs.close();
+	                /*---*/
+	                
 	                //fos.
 	                fos.close();
 	                connectedSock.close();
 	                
 	            } catch (IOException ex) {
 	            	System.out.println("Caught I/O Exception transferring file: "+ex);
-	            }
-	        }
+					System.out.println("IO Error while getting HDFS URI");
+					ex.printStackTrace();
+				//} catch (URISyntaxException e) {
+				//	// TODO Auto-generated catch block
+				//	System.out.println("Error in HDFS URI Syntax, while getting HDFS URI");
+				//	e.printStackTrace();
+				}
+		}
 		//}
 		
 	}
---
+++ src/main/java/net/zwerks/sshshell/HDFSWriter.java
@@ -0,0 +1,48 @@
+package net.zwerks.sshshell;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
+
+
+public class HDFSWriter {
+
+	private String FileOutputPath;
+	private Configuration hdfsConf;
+	
+	
+	public HDFSWriter(String theOutputPath){
+		this.hdfsConf = new Configuration();
+		this.hdfsConf.addResource(new Path("/usr/local/hadoop/conf/core-site.xml"));
+		this.hdfsConf.addResource(new Path("/usr/local/hadoop/conf/hdfs-site.xml"));
+	}
+	
+	public void write(String myOutputPath, Configuration myHDFSConf){
+		
+
+		FileSystem myHDFS = null; // = new FileSystem;
+		try {
+			myHDFS = FileSystem.get(myHDFSConf);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Caught IO Exception while getting HDFS Configuration");
+			e.printStackTrace();
+		}
+
+		
+		Path myPath = new Path(myOutputPath);
+		
+		//Check if file already exists on Output Path
+		try {
+			myHDFS.exists(myPath);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File at " + myPath + " already exists.");
+			e.printStackTrace();
+		}
+	}
+}
